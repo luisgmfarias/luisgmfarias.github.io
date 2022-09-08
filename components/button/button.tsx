efcec562e9821a607c100ceb2270b0e391b73059
luisgmfarias <luisgmfarias@gmail.com>
2022-09-08 11:09:50 -0300

    migration to next and TS

---
+++ components/button/button.tsx
@@ -0,0 +1,6 @@
+export const Button = () => {
+    return (
+        <a>
+        </a>
+    )
+}
---
+++ sections/landing/landing.tsx
@@ -0,0 +1,9 @@
+import { SiIcomoon } from 'react-icons/si'
+
+export const Landing = () => {
+    return (
+        <section>
+            <SiIcomoon />
+        </section>
+    )
+}
